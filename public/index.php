b319b1c99741a150ec93f5b84b9e36dcc4eaa29d
anas saaid <anassaaid@gmail.com>
2019-01-20 13:16:32 +0100

    Create a router

--- public/index.php
+++ public/index.php
@@ -1,3 +1,28 @@
 <?php
 
-echo 'Requested uri = ' . $_SERVER['QUERY_STRING'];
+/**
+ * Front Controller 
+ */
+
+require '../Core/Router.php';
+
+$router = new Router();
+
+$router->add('',[
+	'controller' => 'Home',
+	'action' => 'index'
+]);
+
+$router->add('/posts',[
+	'controller' => 'Posts',
+	'action' => 'index'
+]);
+
+$router->add('/posts/new',[
+	'controller' => 'Posts',
+	'action' => 'new'
+]);
+
+echo "<pre>";
+var_dump($router->getRoutes());
+echo "</pre>";
