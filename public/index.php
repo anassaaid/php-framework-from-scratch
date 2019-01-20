a2be33f00a35e8d25c230a92788ae917cfd832fe
anas saaid <anassaaid@gmail.com>
2019-01-20 20:35:14 +0100

    Controllers : Add Post controller and create the dispatcher method in the Router

--- public/index.php
+++ public/index.php
@@ -4,6 +4,9 @@
  * Front Controller 
  */
 
+// 	Require the controller class
+require '../App/Controllers/Posts.php';
+//	Require the router class
 require '../Core/Router.php';
 
 $router = new Router();
@@ -16,16 +19,5 @@ $router->add('',[
 $router->add('{controller}/{action}');
 $router->add('{controller}/{id:\d+}/{action}');
 $router->add('admin/{action}/{controller}');
-// echo '<pre>';
-// echo htmlspecialchars(print_r($router->getRoutes(), true));
-// echo '</pre>';
 
-$url = $_SERVER['QUERY_STRING'];
-
-if($router->match($url)){
-	echo "<pre>";
-	var_dump($router->getParams());
-	echo "</pre>";
-}else{
-	echo "No route found for url '" . $url . "'";
-}
+$router->dispatch($_SERVER['QUERY_STRING']);
