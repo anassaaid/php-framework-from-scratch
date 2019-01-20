0b72c51bcacf6037187737fe36f52d49c0ea20dc
anas saaid <anassaaid@gmail.com>
2019-01-20 14:41:13 +0100

    Add the routes array and look for matching urls

--- Core/Router.php
+++ Core/Router.php
@@ -6,12 +6,28 @@
 
 class Router{
 	protected $routes = [];
+	protected $params = [];
 
 	public function add($route, $params){
 		$this->routes[$route] = $params;
 	}
 
-	public function getRoutes($value=''){
+	public function match($url){
+		foreach ($this->routes as $route => $params) {
+			if($url == $route){
+				$this->params = $params;
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	public function getParams(){
+		return $this->params;
+	}
+
+	public function getRoutes(){
 		return $this->routes;
 	}
 }
