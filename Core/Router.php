f40e8c9833fd9e75d1dd363b394398a43c1c28fe
anas saaid <anassaaid@gmail.com>
2019-01-20 21:37:20 +0100

    Remove query string variables from the URL before matching to a route

--- Core/Router.php
+++ Core/Router.php
@@ -53,6 +53,8 @@ class Router{
 
 	public function dispatch($url)
     {
+    	$url = $this->removeQueryStringVariables($url);	
+
         if ($this->match($url)) {
             $controller = $this->params['controller'];
             $controller = $this->convertToStudlyCaps($controller);
@@ -76,6 +78,21 @@ class Router{
         } else {
             echo 'No route matched.';
         }
+    }
+
+    protected function removeQueryStringVariables($url)
+    {
+        if ($url != '') {
+            $parts = explode('&', $url, 2);
+
+            if (strpos($parts[0], '=') === false) {
+                $url = $parts[0];
+            } else {
+                $url = '';
+            }
+        }
+
+        return $url;
     }
 
     protected function convertToStudlyCaps($string)
