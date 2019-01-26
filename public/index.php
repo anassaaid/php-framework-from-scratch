46bad9e576b44af6043f81024173342aa81bd0ad
anas saaid <anassaaid@gmail.com>
2019-01-26 19:06:48 +0100

    Replace the autoload function with the composer autoloader

--- public/index.php
+++ public/index.php
@@ -12,18 +12,6 @@ require "../vendor/autoload.php";
 Twig_Autoloader::register();
 
 /**
- * Autoloader
- */
-spl_autoload_register(function ($class) {
-    $root = dirname(__DIR__);   // get the parent directory
-    $file = $root . '/' . str_replace('\\', '/', $class) . '.php';
-    if (is_readable($file)) {
-        require $root . '/' . str_replace('\\', '/', $class) . '.php';
-    }
-});
-
-
-/**
  * Routing
  */
 $router = new Core\Router();
