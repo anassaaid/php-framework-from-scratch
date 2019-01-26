d81325a2108a9739d4bce1358242cb9cd8410dab
anas saaid <anassaaid@gmail.com>
2019-01-26 23:03:34 +0100

    Put application configuration settings in a separate file

--- Core/Model.php
+++ Core/Model.php
@@ -2,6 +2,7 @@
 namespace Core;
 
 use PDO;
+use App\Config;
 
 abstract class Model{
 
@@ -14,8 +15,9 @@ abstract class Model{
             $password = '';
     
             try {
-                $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", 
-                              $username, $password);
+            	$dsn = "mysql:host=" . Config::DB_HOST . ";dbname=" . 
+            			Config::DB_NAME . ";charset=utf8";
+                $db = new PDO($dsn,Config::DB_USER, Config::DB_PASSWORD);
 
             } catch (PDOException $e) {
                 echo $e->getMessage();
